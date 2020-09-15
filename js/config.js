6985bb19fd8577efb1527274e483011a26d8d616
shai5050 <shai.isaiah@gmail.com>
2020-09-15 01:41:53 +0000

    Configure cognito

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_STKliYUTH', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '3l2fml6oo9ql17f7ki6e99697j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
